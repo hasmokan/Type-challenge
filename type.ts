a4a7bf3cd7e35bb128945e7c426960df1d7354b2
hasmokan <1021056159@qq.com>
2024-07-14 00:05:27 +0800

    flatten

--- type.ts
+++ type.ts
@@ -7,10 +7,20 @@ type Permutation<T, K = T> = [T] extends [never]
 /**
  * medium
  * https://github.com/type-challenges/type-challenges/blob/main/questions/00298-medium-length-of-string/README.md
-*/
+ */
 type LengthOfString<
 	S extends string,
 	T extends string[] = []
 > = S extends `${infer first}${infer rest}`
 	? LengthOfString<rest, [...T, first]>
 	: T["length"];
+
+type Flatten<T extends any[]> = T extends [infer first, ...infer rest]
+	? first extends any[]
+		? Flatten<[...first, ...rest]>
+		: [first, ...Flatten<rest>]
+	: T;
+
+
+    
+type result = Flatten<[1, 2, [3, 4], [[[5]]]]>; 
