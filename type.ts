ed4b5c13f1e5e4ac3dba8cea7c9e1c5aa9ba6650
hasmokan <1021056159@qq.com>
2024-07-13 23:35:19 +0800

    lengthofString

--- type.ts
+++ type.ts
@@ -1,6 +1,12 @@
-type Permutation<T, K=T> =
-    [T] extends [never]
-      ? []
-      : K extends K
-        ? [K, ...Permutation<Exclude<T, K>>]
-        : never
+type Permutation<T, K = T> = [T] extends [never]
+	? []
+	: K extends K
+	? [K, ...Permutation<Exclude<T, K>>]
+	: never;
+
+type LengthOfString<
+	S extends string,
+	T extends string[] = []
+> = S extends `${infer first}${infer rest}`
+	? LengthOfString<rest, [...T, first]>
+	: T["length"];
