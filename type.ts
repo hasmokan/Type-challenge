ebb7876dbf75b388bfd2254bfecbe743c30ecfe2
hasmokan <1021056159@qq.com>
2024-07-13 23:36:26 +0800

    feat: LENGTHOFSTRING

--- type.ts
+++ type.ts
@@ -4,6 +4,10 @@ type Permutation<T, K = T> = [T] extends [never]
 	? [K, ...Permutation<Exclude<T, K>>]
 	: never;
 
+/**
+ * medium
+ * https://github.com/type-challenges/type-challenges/blob/main/questions/00298-medium-length-of-string/README.md
+*/
 type LengthOfString<
 	S extends string,
 	T extends string[] = []
